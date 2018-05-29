0d7942d79c8ec1851922485fc3d2fe808ff81467
dreed <derek.paul.reed@gmail.com>
2018-05-29 08:43:20 -0400

    day 5 excersizes

--- m1-w1d5-command-line-programs-exercises/src/main/java/com/techelevator/Fibonacci.java
+++ m1-w1d5-command-line-programs-exercises/src/main/java/com/techelevator/Fibonacci.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  The Fibonacci numbers are the integers in the following sequence:  
 	0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
@@ -17,7 +19,24 @@ Please enter the Fibonacci number: 25
 public class Fibonacci {
 
 	public static void main(String[] args) {
-
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Please enter an integer: ");
+		String fibNumber = in.nextLine();
+		int myFibNumber = Integer.parseInt(fibNumber);
+		
+		int[] fib = new int [myFibNumber];
+		fib[0] = 0;
+		fib[1] = 1;
+		for (int i = 2; i < myFibNumber; i++) {
+			fib [i] = fib[i-1] + fib[i-2];
+		}
+		
+		for (int i : fib) {
+			System.out.println(i);
+		}
+		
 	}
 
 }
--- m1-w1d5-command-line-programs-exercises/src/main/java/com/techelevator/LinearConvert.java
+++ m1-w1d5-command-line-programs-exercises/src/main/java/com/techelevator/LinearConvert.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  The foot to meter conversion formula is:
  	m = f * 0.3048
@@ -19,7 +21,33 @@ package com.techelevator;
 public class LinearConvert {
 
 	public static void main(String[] args) {
-
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Please enter a length: ");
+		
+		String input = in.nextLine();
+		double amount = Double.parseDouble(input);
+		double mToFeet = (amount * 0.3048);
+		double fToMetr = (amount * 3.2808399);
+		
+//		System.out.println(input);
+		
+		System.out.println("Please enter (F)eet or (M)eters: ");
+		
+		String choice = in.nextLine();
+		
+//		System.out.println(choice);
+		
+		System.out.println("You've entered " + input + choice + "!");
+		
+		switch(choice) {
+			case "F":
+				System.out.println("The length in Meters is " + fToMetr);
+				break;
+			case "M":
+				System.out.println("The length in Feet is " + mToFeet);
+				break;
+		}
 	}
-
 }
