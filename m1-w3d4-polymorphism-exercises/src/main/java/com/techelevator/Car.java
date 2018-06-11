f147a016be692369aca98042bbc29f645c39035a
dreed <derek.paul.reed@gmail.com>
2018-06-11 08:53:49 -0400

    w3d4

---
+++ m1-w3d4-polymorphism-exercises/src/main/java/com/techelevator/Car.java
@@ -0,0 +1,11 @@
+package com.techelevator;
+
+public class Car implements Vehicle {
+
+	@Override
+	public double calculateToll(int distance) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ m1-w3d4-polymorphism-exercises/src/main/java/com/techelevator/Vehicle.java
@@ -0,0 +1,7 @@
+package com.techelevator;
+
+public interface Vehicle {
+	
+	public double calculateToll (int distance);
+
+}
