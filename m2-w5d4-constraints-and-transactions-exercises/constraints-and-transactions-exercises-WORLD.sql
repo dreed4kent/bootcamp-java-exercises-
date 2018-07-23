34335b172200de8da75ac3522472315d3be4bf3d
dreed <derek.paul.reed@gmail.com>
2018-07-22 22:24:28 -0400

    w5d4

--- m2-w5d4-constraints-and-transactions-exercises/constraints-and-transactions-exercises-WORLD.sql
+++ m2-w5d4-constraints-and-transactions-exercises/constraints-and-transactions-exercises-WORLD.sql
@@ -1,32 +1,70 @@
--- Write queries to return the following:
+9-- Write queries to return the following:
 -- Make the following changes in the "world" database.
 
 -- 1. Add Superman's hometown, Smallville, Kansas to the city table. The 
 -- countrycode is 'USA', and population of 45001. (Yes, I looked it up on 
 -- Wikipedia.)
 
+INSERT INTO city (id, name, countrycode, district, population)
+VALUES ('4080', 'Smallville', 'USA', 'Kansas', '45001');
+
 -- 2. Add Kryptonese to the countrylanguage table. Kryptonese is spoken by 0.0001
 -- percentage of the 'USA' population.
+
+INSERT INTO countrylanguage (language, countrycode, isofficial, percentage)
+VALUES ('Kryptonese', 'USA',false, 0.0001)
 
 -- 3. After heated debate, "Kryptonese" was renamed to "Krypto-babble", change 
 -- the appropriate record accordingly.
 
+UPDATE countrylanguage
+SET language = 'Krypto-babble'
+WHERE language = 'Kryptonese';
+
 -- 4. Set the US captial to Smallville, Kansas in the country table.
+
+UPDATE country
+SET capital = 4080
+WHERE code = 'USA'
 
 -- 5. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
 
+DELETE
+FROM city                       
+WHERE id = 4080;
+
 -- 6. Return the US captial to Washington.
 
+UPDATE country
+SET capital = 3813
+WHERE code = 'USA'
+
 -- 7. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
+
+DELETE FROM city
+WHERE id = 4080;
 
 -- 8. Reverse the "is the official language" setting for all languages where the
 -- country's year of independence is within the range of 1800 and 1972 
 -- (exclusive). 
 -- (590 rows affected)
 
+UPDATE countrylanguage
+SET isofficial = NOT isofficial
+FROM country
+WHERE country.code = countrylanguage.countrycode AND country.indepyear BETWEEN 1800 AND 1972
+
 -- 9. Convert population so it is expressed in 1,000s for all cities. (Round to
 -- the nearest integer value greater than 0.)
 -- (4079 rows affected)
+
+SELECT population
+FROM city
+where population < 1000
+
+UPDATE city
+SET population = population / 1000
+WHERE population > 1
 
 -- 10. Assuming a country's surfacearea is expressed in miles, convert it to 
 -- meters for all countries where French is spoken by more than 20% of the 
