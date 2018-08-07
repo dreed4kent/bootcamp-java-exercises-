8183fbee087f6256c26d223cd91ba05b02d40993
dreed <derek.paul.reed@gmail.com>
2018-08-07 13:52:52 -0400

    w10d2

--- m4-w10d2-java-history_geek_exercises/src/main/webapp/js/exercises/api-exercises.js
+++ m4-w10d2-java-history_geek_exercises/src/main/webapp/js/exercises/api-exercises.js
@@ -1,10 +1,11 @@
 ﻿/// <reference path="../jquery-3.1.1.js" />
 $(document).ready(function () {
 	 $("#BillingPostalCode").on("change", function (event) {
-		 var billingZipCode = $("#BillingPostalCode").val();
+		 var zipCode = $("#BillingPostalCode").val();
 		 var subtotal = $("#subtotal .price").text().substring(1);
 	        $.ajax({
-	            url: "http://localhost:8080/m4-java-historygeek-exercises/api/getTax?billingZipCode=" + billingZipCode + "&subtotal=" + subtotal,
+	            url: "http://localhost:8080/m4-java-historygeek-exercises/api/getTax",
+	            data: {billingZipCode: zipCode, subtotal: subtotal},
 	            type: "GET",
 	            dataType: "json"
 	        }).done(function (data) {
